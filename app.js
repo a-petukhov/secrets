6b15e04be948d443d20424dd13dab03d1682be14
Oleksii Petukhov <48577654+a-petukhov@users.noreply.github.com>
2022-07-23 00:33:23 +0300

    Updating the app user management to security lvl5

--- app.js
+++ app.js
@@ -4,25 +4,68 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+const session = require('express-session');
+const {
+    request
+} = require('http');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static('public'));
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 mongoose.connect(process.env.MONGO);
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    active: Boolean
 });
+
+userSchema.plugin(passportLocalMongoose, {
+    // Set usernameUnique to false to avoid a mongodb index on the username column!
+    usernameUnique: false,
+  
+    findByUsername: function(model, queryParameters) {
+      // Add additional query parameter - AND condition - active: true
+      queryParameters.active = true;
+      return model.findOne(queryParameters);
+    }
+  });
 
 const User = new mongoose.model('User', userSchema);
 
+// CHANGE: USE "createStrategy" INSTEAD OF "authenticate"
+passport.use(User.createStrategy());
+
+// use static serialize and deserialize of model for passport session support
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.route('/secrets')
+.get((req, res) => {
+    if (req.isAuthenticated()) {
+        res.render('secrets');
+    } else {
+        res.redirect('/login');
+    }
 });
 
 app.route('/login')
@@ -30,39 +73,46 @@ app.route('/login')
         res.render('login');
     })
     .post((req, res) => {
-        User.findOne({email: req.body.username}, (err, result) => {
-            if (err) {console.log(err);}
-            else {
-                if (result) {
-                    if (result.password == req.body.password) {
-                        res.render('secrets');}
-                    else {
-                        console.log('Password doesn\'t match.');
-                        res.redirect('/login');
-                    }
-                } else {
-                    console.log('User not found.');
-                        res.redirect('/login');
-                }
-                }});
-});
-
+        const user = new User({
+            username: req.body.username,
+            password: req.body.password
+        });
+        req.login(user, (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                passport.authenticate('local')(req, res, () => {
+                    res.redirect('/secrets');
+                });
+            }
+        });
+    });
 
 app.route('/register')
     .get((req, res) => {
         res.render('register');
     })
     .post((req, res) => {
-        const newUser = new User ({
-            email: req.body.username,
-            password: md5(req.body.password)
-        });
+        User.register({username: req.body.username, active: true}, req.body.password, function(err, user) {
+            if (err) {
+                console.log(err);
+                res.redirect('/register');
+            } else {
+                passport.authenticate('local')(req, res, () => {
+                    res.redirect('/secrets');
+                });
+        }});
+    });
 
-        newUser.save((err) => {
-            if (err) {console.log(err);}
-            else {res.render('secrets');}
-        });
+app.get('/logout', (req, res) => {
+    req.logout((err) => {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect('/');
+    });
 });
+
 
 let port = process.env.PORT;
 if (port == null || port == '') {
@@ -70,5 +120,5 @@ if (port == null || port == '') {
 }
 
 app.listen(port, (req, res) => {
-    console.log('Server is now running at port ' + port +'.');
+    console.log('Server is now running at port ' + port + '.');
 });
