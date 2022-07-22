9995240c52d10f40f5a6a37f00905595339bcb83
Oleksii Petukhov <48577654+a-petukhov@users.noreply.github.com>
2022-07-22 18:48:18 +0300

    Adding encryption for the user's password

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -9,16 +11,59 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+mongoose.connect('mongodb://localhost:27017/secretsDB');
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = 'This is our little secret.';
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+
+const User = new mongoose.model('User', userSchema);
+
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/login', (req, res) => {
-    res.render('login');
+app.route('/login')
+    .get((req, res) => {
+        res.render('login');
+    })
+    .post((req, res) => {
+        User.findOne({email: req.body.username}, (err, result) => {
+            if (err) {console.log(err);}
+            else {
+                if (result) {
+                    if (result.password == req.body.password) {
+                        res.render('secrets');}
+                    else {
+                        console.log('Password doesn\'t match.');
+                        res.redirect('/login');
+                    }
+                } else {
+                    console.log('User not found.');
+                        res.redirect('/login');
+                }
+                }});
 });
 
-app.get('/register', (req, res) => {
-    res.render('register');
+
+app.route('/register')
+    .get((req, res) => {
+        res.render('register');
+    })
+    .post((req, res) => {
+        const newUser = new User ({
+            email: req.body.username,
+            password: req.body.password
+        });
+
+        newUser.save((err) => {
+            if (err) {console.log(err);}
+            else {res.render('secrets');}
+        });
 });
 
 let port = process.env.PORT;
